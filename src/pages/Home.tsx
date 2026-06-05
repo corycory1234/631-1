9adc7f1d5bc712605cd23ecc0319f0e3d154ad00
Cory <note10litenote10lite@gmail.com>
2026-06-05 23:11:11 +0800

    fix: Test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,5 +2,6 @@ export default function Home () {
   return <>
     <h1 className='text-red-500'>CSS</h1>
     <p className="text-5xl">256</p>
+    <p className="text-amber-400 text-6xl">TEST</p>
   </>
 }
